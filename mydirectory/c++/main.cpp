b9aa839e7a99eeec36898cb4e7687d0d0a96b2c8
kashish138 <146374962+kashish138@users.noreply.github.com>
2024-08-27 22:20:31 +0530

    Delete mydirectory directory

--- mydirectory/c++/main.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Kashish";
-    return 0;
-}
--- mydirectory/tut65.js
+++
@@ -1,6 +0,0 @@
-// Synchronous or blocking
-// -line by line execution
-
-// Asynchronous or blocking
-// -line by line execution not guaranteed
-// -callbacks will fire
